e24307a3ce9af678fb5df9f69434ad878df11262
Michael Shiver <michael.shiver@ascte.org>
2026-09-09 10:57:05 -0500

    updated goblin

--- goblin.py
+++ goblin.py
@@ -1,9 +1,11 @@
 from fighter import Fighter
 
 class Goblin(Fighter):
-    """A completed character class students can examine as an OOP example."""
+ # The basic enemy, fairly weak, ut stronger in groups
 
     def __init__(self, name):
         super().__init__(name)
+        self.health = 50
+        self.attack_power = 10
 
 
