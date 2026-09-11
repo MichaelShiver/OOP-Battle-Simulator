0e5eafe7bebe16c6f75a72d8448d0f4e4cce6e35
Michael Shiver <michael.shiver@ascte.org>
2026-09-11 08:30:25 -0500

    Fixed Comment

--- goblin.py
+++ goblin.py
@@ -1,7 +1,7 @@
 from fighter import Fighter
 
 class Goblin(Fighter):
- # The basic enemy, fairly weak, ut stronger in groups
+    # The basic enemy, fairly weak, but stronger in groups
 
     def __init__(self, name):
         super().__init__(name)
