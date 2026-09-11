ce31f2652db5aa898b3b209476b685434a3a1d3d
Michael Shiver <michael.shiver@ascte.org>
2026-09-11 08:49:30 -0500

    Revert "updated goblin"
    This reverts commit e24307a3ce9af678fb5df9f69434ad878df11262.

--- fighter.py
+++ fighter.py
@@ -3,7 +3,6 @@ import random
 "Overarchingn class that fighters inherit from"
 
 class Fighter:
-    # A completed character class students can examine as an OOP example.
 
     def __init__(self, name):
         self.name = name
--- goblin.py
+++ goblin.py
@@ -1,11 +1,9 @@
 from fighter import Fighter
 
 class Goblin(Fighter):
- # The basic enemy, fairly weak, ut stronger in groups
+    """A completed character class students can examine as an OOP example."""
 
     def __init__(self, name):
         super().__init__(name)
-        self.health = 50
-        self.attack_power = 10
 
 
